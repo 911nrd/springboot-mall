cabb9e517555ba279c8fa39433c64f0b6a7a4c0a
Lary <s0970227@gmail.com>
2024-08-31 21:57:09 +0800

    商品功能 - 實作查詢商品列表

--- src/main/java/com/lary/springbootmall/dao/ProductDao.java
+++ src/main/java/com/lary/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.lary.springbootmall.dao;
 import com.lary.springbootmall.dto.ProductRequest;
 import com.lary.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/lary/springbootmall/service/ProductService.java
+++ src/main/java/com/lary/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.lary.springbootmall.service;
 import com.lary.springbootmall.dto.ProductRequest;
 import com.lary.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
@@ -11,4 +15,6 @@ public interface ProductService {
     void updateProduct(Integer productId, ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
+
 }
