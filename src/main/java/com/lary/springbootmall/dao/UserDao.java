1692223e33b969d3c4e7b3d71a26acd9db8cab73
Lary <s0970227@gmail.com>
2025-01-12 20:12:16 +0800

    帳號功能 - 檢查註冊的email和隱藏密碼

--- src/main/java/com/lary/springbootmall/dao/UserDao.java
+++ src/main/java/com/lary/springbootmall/dao/UserDao.java
@@ -7,7 +7,11 @@ public interface UserDao {
 
     User getUserById(Integer userId);
 
+    User getUserByEmail(String email);
+
     Integer createUser(UserRegisterRequest userRegisterRequest);
+
+
 
 
 }
